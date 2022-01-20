277b40cbcc1b7edd91414be05b18e6bf3e4a98b1
ahn seoyeon <ahnsun98@gmail.com>
2022-01-21 04:33:03 +0900

    delete

--- openAPIdata/mountain_info/protectKey/set.py
+++
@@ -1,7 +0,0 @@
-import json
-from collections import OrderedDict
-
-data = OrderedDict()
-data['key'] = 'w91CC6XKX5C3vgOonc8ZN+udXJehNQLYRtMbpuDf8wWgPXPhrB25+2uoYanXzArao/m4Ldi7yNqgiQY4IuA7PQ=='
-with open('protectKey/key.json','w',encoding='utf-8') as file:
-    json.dump(data, file, indent='\t')
