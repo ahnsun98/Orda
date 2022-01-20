8001aa90586e3c5e5d03ecfa274dac72d0a84306
ahn seoyeon <ahnsun98@gmail.com>
2022-01-20 17:00:40 +0900

    modify INSTALLED_APPS

--- orda/config/settings.py
+++ orda/config/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
+    'region',
+    'info',
+    'infoplus',
+    'review',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
