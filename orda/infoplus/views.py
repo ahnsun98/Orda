42a4e3a724ed866f5da5c28f1fb90054808d402e
kwonwookhyun <kwenll@naver.com>
2022-01-24 15:01:56 +0900

    first_wook

--- orda/infoplus/views.py
+++ orda/infoplus/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+import json
 
 # Create your views here.
+
+def food(request):
+    return render(request, 'infoplus/food.html')    
+
