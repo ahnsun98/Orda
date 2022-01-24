d804baa44eec60947b9bb0ace5547d188a7d664e
kyl5170 <kyl5170@naver.com>
2022-01-25 01:16:23 +0900

    create location marker & infowindow

--- orda/config/urls.py
+++ orda/config/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from region import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('info/', include('info.urls')),
+    path('region/', include('region.urls')),
 ]
--- orda/region/views.py
+++ orda/region/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from . models import *
+
+def showmap(request):
+    mt_list=Mountain.objects.all()
+    return render(request, 'region/regionInfo.html', {'mt_list':mt_list})
