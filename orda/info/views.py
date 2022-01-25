74465ddb86b5444d617d81a89c56b116a9152189
ahn seoyeon <ahnsun98@gmail.com>
2022-01-25 23:37:19 +0900

    modify DB

--- orda/info/views.py
+++ orda/info/views.py
@@ -21,7 +21,7 @@ def main(request):
     return render(request, 'info/infopage.html', context)
 
 def list(request):
-    filter1 = int(request.GET.get('filter',3))
+    filter1 = int(request.GET.get('filter',1))
     print(filter1)
     if(filter1 == 1): #높이 높은순
         filters = '-height'
