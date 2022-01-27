44404e7a930ea23682a24ad0c8abcb42dd8fdad2
notsalhoon <keroro10ha@naver.com>
2022-01-27 14:46:08 +0900

    mm

--- orda/review/views.py
+++ orda/review/views.py
@@ -18,13 +18,9 @@ def post(request):
         m_name = request.POST['m_name']
         m_loc = request.POST['m_loc']
 
-<<<<<<< HEAD
         file = request.FILE.get('file')
 
         post = Post(author=author, title=title, contents=contents, m_name=m_name, m_loc=m_loc, file=file)
-=======
-        post = Post(author=author, title=title, contents=contents, m_name=m_name, m_loc=m_loc)
->>>>>>> notsalhoon2
 
         post.save()
         return HttpResponseRedirect('/review/')
@@ -50,10 +46,7 @@ def edit(request,id):
         post.contents = request.POST['contents']
         post.m_name = request.POST['m_name']
         post.m_loc = request.POST['m_loc']
-<<<<<<< HEAD
         post.file = request.FILE['file']
-=======
->>>>>>> notsalhoon2
 
         post.save()
         return HttpResponseRedirect('/review/')
